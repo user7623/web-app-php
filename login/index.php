57cf0ba535e9f6509a63aedb3b7c33558a032236
user7623 <ddroid172@gmail.com>
2020-04-10 21:59:59 +0200

    first commit

---
+++ login/index.php
@@ -0,0 +1,51 @@
+<?php
+$msg = '';
+//proveri dali se postaveni username i password i dali e stegnato login
+if(isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password']))
+{
+    //proveri dali se tocnni
+    //ova treba pokasno so funckii za proverka od databaza i da se koristat klasi
+    if($_POST['username'] == 'username' && $_POST['password'] == 'password')
+    {
+         $_SESSION['valid'] = true;
+         $_SESSION['timeout'] = time();
+         $_SESSION['username'] = $_POST['username'];
+        $msg = 'login success';
+    }
+    else
+    {
+        $msg = 'invalid username or password';
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <div class="head_div">
+            <h1>Welcome to # Site name #</h1>
+        </div>
+    </head>
+    <body>
+        <div class="wrapper">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <?php if($msg != '') : ?>
+                <p class="error"><?php echo $msg ?></p>
+                <?php endif; ?>
+                <label>Username</label><br>
+                <input type="text" name="username" placeholder="username here" required autofocus><br>
+                <label>Password</label><br>
+                <input type="text" name="password" placeholder="password here" required><br><br>
+                
+                <button class="basic_button" type="submit" name="login">Log in</button>
+            </form>
+            <form method="post" action="register.php">
+            <div>
+                <button class="basic_button_2" type="submit" name="register">Register</button>
+            </div>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ login/register.php
@@ -0,0 +1,64 @@
+<?php
+$msg = '';
+$pass = filter_input(INPUT_POST, 'password');
+$pass_confirm = filter_input(INPUT_POST, 'password_confirm');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+//proveri dali e vnesen validen meil
+
+if($email !== true && !empty($pass))
+{
+    $msg = 'Please input valid email';
+}
+
+//proveri dali e unikatno username
+
+//proveri dali se sovpagjaad passwords
+if($pass != $pass_confirm)
+{
+    $msg = 'password inputs not matching';
+}
+
+// ako se e vo red prati meil 
+
+if($msg == '' && !empty($pass))
+{
+    $msg = 'cekam_avtentikacija';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <div class="head_div">
+            <h1>Register to # Site name #</h1>
+        </div>
+    </head>
+    <body>
+        <div class="wrapper">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <?php if($msg == 'cekam_avtentikacija') : ?>
+                <p>Check inbox for conformation! NOW!!!</p>
+                <div>
+                    <!-- ova moze mn podobro -->
+                    <a href="http://localhost:84/web_php/login" class="button">Back to log in</a>
+                </div>
+                <?php else : ?>
+                    <?php if($msg != '') : ?>
+                    <p class="error"><?php echo $msg ?></p>
+                    <?php endif; ?>
+                    <label>Username</label><br>
+                    <input type="text" name="username" placeholder="username here" required autofocus><br>
+                    <label>Password</label><br>
+                    <input type="text" name="password" placeholder="password here" required><br><br>
+                    <label>Confirm Password</label><br>
+                    <input type="text" name="password_confirm" placeholder="repeat password here" required><br>
+                    <label>Email</label><br>
+                    <input type="text" name="email" placeholder="Your email" required><br>
+
+                    <button class="basic_button" type="submit" name="register_new_user">Register</button>
+                <?php endif; ?>
+                   
+            </form>
+        </div>
+    </body>
+</html>
